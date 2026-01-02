6195adc840cb6fc0ef6930b79b849fb8186e772d
Mariana Rafaela <limasmariana.souza@gmail.com>
2026-01-01 21:39:40 -0300

    feat: add GenAI integration, audio transcription and questions endpoints

--- back/server/server.ts
+++ back/server/server.ts
@@ -1,4 +1,5 @@
 import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
 import { fastify } from 'fastify'
 import {
   serializerCompiler,
@@ -9,14 +10,17 @@ import { env } from './env.ts'
 
 import { createQuestionRoute } from './routes/create-questions.ts'
 import { createRoomRoute } from './routes/create-rooms.ts'
-import { getRoomQuestions } from './routes/get-questions.ts'
+import { getQuestionsRoute } from './routes/get-questions.ts'
 import { getRoomsRoute } from './routes/get-rooms.ts'
+import { uploadAudioRoute } from './routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
   origin: 'http://localhost:5173',
 })
+
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -25,7 +29,8 @@ app.get('/health', () => 'OK')
 
 app.register(getRoomsRoute)
 app.register(createRoomRoute)
-app.register(getRoomQuestions)
+app.register(getQuestionsRoute)
 app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
 
 app.listen({ port: env.PORT })
